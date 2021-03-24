14fe2a7d127418dbaa1dae2ca3fa55a542caae14
Daniil Novoselov <gudnmail@gmail.com>
2021-03-24 13:52:53 +0500

    add gokgs worker

--- gokgs/worker.py
+++ gokgs/worker.py
@@ -1,22 +1,28 @@
+import asyncio
 import copy
 import os
+import sys
+import time
+import json
 
 import httpx
 from retrying_async import retry
 
 from gokgs.app import app
+from gokgs.redis import r
 
 URL = 'https://www.gokgs.com/json/access'
+_name = _password = _task = None
 client = httpx.AsyncClient()
 
 
-@retry(attempts=3, delay=3)
+@retry(attempts=3, delay=1)
 async def post(action, body):
     body = copy.deepcopy(body)
     body['type'] = action.upper()
-    r = await client.post(URL, json=body)
-    r.raise_for_status()
-    return r.text
+    resp = await client.post(URL, json=body)
+    resp.raise_for_status()
+    return resp.text
 
 
 async def login(name, password):
@@ -27,15 +33,49 @@ async def login(name, password):
     })
 
 
+async def get():
+    resp = await client.get(URL)
+    resp.raise_for_status()
+    try:
+        return resp.json()
+    except json.JSONDecodeError:
+        return {}
+
+
+async def loop_worker():
+    redis = r()
+    try:
+        while True:
+            try:
+                resp = await get()
+                msgs = resp.get('messages', [])
+                for msg in msgs:
+                    t = msg.get('type', 'UNKNOWN_TYPE')
+                    key = f'{t}_{time.time()}'
+                    await redis.set(key, json.dumps(msg), expire=3600)
+            except asyncio.CancelledError:
+                raise
+            except httpx.ReadTimeout:
+                pass
+            except Exception as e:
+                await login(_name, _password)
+    except asyncio.CancelledError:
+        pass
+
+
 @app.on_event('shutdown')
 async def shutdown():
+    if _task:
+        _task.cancel()
     await client.aclose()
 
 
 @app.on_event('startup')
 async def init():
-    name = os.getenv('NAME')
-    password = os.getenv('PASSWORD')
-    if not name or not password:
+    global _name, _password, _task
+    _name = os.getenv('NAME')
+    _password = os.getenv('PASSWORD')
+    if not _name or not _password:
         return
-    await login(name, password)
+    await login(_name, _password)
+    _task = asyncio.create_task(loop_worker())
